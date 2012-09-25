2f98e40fb621c343048b7f966407961c5af4e2f7
entrix <entrix.91@gmail.com>
2012-09-26 01:29:18 +0400

    add business logic without complete testing all cases

---
+++ src/main/java/org/hibernate/bpla/Main.java
@@ -0,0 +1,26 @@
+package org.hibernate.bpla;
+
+import org.hibernate.bpla.persistence.BplaService;
+import org.hibernate.bpla.util.HibernateUtil;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: entrix
+ * Date: 16.03.2012
+ * Time: 23:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BplaService mgr = new BplaService();
+
+        if (args[0].equals("fly")) {
+            //mgr.createAndStoreEvent("My Detail", new Date());
+        }
+        else if (args[0].equals("list")) {
+            mgr.printAll();
+        }
+
+        HibernateUtil.close();
+    }
+}
